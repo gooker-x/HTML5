12983a58123ae165594b9c9462b007d488adb5dc
xu_fnym <48125762+gooker-x@users.noreply.github.com>
2021-05-20 18:23:03 +0800

    Add files via upload

---
+++ get.php
@@ -0,0 +1,15 @@
+
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<title></title>
+		</head>
+		<body>
+			<?php
+					
+					$result = file_get_contents("class.html");
+					echo $result;
+			?>
+		</body>
+	</html>
+	
